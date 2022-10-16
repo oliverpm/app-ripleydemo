9fac9fae46001b443fe511251988211365427111
opmeza <opmeza@stefanini.com>
2022-10-16 17:51:46 -0500

    adding Ip and Name POD

--- src/main/java/com/oliver/ripley/controller/homeController.java
+++ src/main/java/com/oliver/ripley/controller/homeController.java
@@ -2,12 +2,24 @@ package com.oliver.ripley.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
 
 @RestController
 public class homeController {
-	
+
 	@GetMapping("/")
 	public String inicio() {
-		return "Hola Ripley!!.";
+		String name="", address="";
+		try {
+			InetAddress inetadd = InetAddress.getLocalHost();
+			name = inetadd.getHostName();
+			address = inetadd.getHostAddress();
+		}
+		catch(Exception e){
+		}
+		return "\n Hola Banco Ripley!!! \n" +
+				"<br> \n" +
+				"<br> ==> POD: Host Name: " + name + "\n" +
+				"<br> ==> POD: Ip Address: " + address + "\n";
 	}
 }
